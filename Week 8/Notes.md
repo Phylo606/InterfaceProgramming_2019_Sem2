0c086c982ab311f529426322ca64788942ea584c
MATTHEW PHYLAND <102119144@student.swin.edu.au>
2019-09-17 15:13:38 +1000

    tic-tac-toe-initial

---
+++ Week 8/Notes.md
@@ -0,0 +1,1 @@
+Look into 2 dimensional array and display: inline-block
---
+++ Week 8/tic-tac-toe-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {my-board} from 'board.component.ts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tic-tac-toe-app';
+}
